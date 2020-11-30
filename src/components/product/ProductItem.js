92337a08fe5b059e23d1067dc7be342a0be02149
Bruno Austin <brunoaugustine2@gmail.com>
2020-11-30 17:56:05 +0100

    add show and hide functionality

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import CartContext from '../../context/cart/CartContext'
 import { formatNumber } from './utils';
 
  const ProductItem = ({product}) => {
+     const [show, setShow] = useState(false)
     const cartContext = useContext(CartContext)
 
     const { addProduct, cartItems, increase } = cartContext
@@ -11,28 +12,41 @@ import { formatNumber } from './utils';
     const isInCart = product => {
         return !!cartItems.find(item => item.id === product.id);
     }
+
+    const handleHide = () => {
+        if(show === false){
+            setShow(true)
+        } else{
+            setShow(false)
+        }
+        
+    }
         return (
             <div className='card card-body'>
                 <h1>{product.category}</h1>
+
                  <img style={{display: "block", margin: "0 auto 10px", maxHeight: "200px"}} className="img-fluid" 
-            src={product.image + '?v=' + product.id} alt=""/>
+                src={product.image + '?v=' + product.id} alt=""/>
+
                 <p>{product.title}</p>
                 <h3 className="text-left">{formatNumber(product.price)}</h3>
-                <p>{product.description}</p>
+                {show ? <p>{product.description}</p> : null}
+                
 
                 <div className="text-right">
-                <Link to="/" className="btn btn-link btn-sm mr-2">Details</Link>
-                { isInCart(product) && 
-                    <button 
-                    onClick={() => increase(product)}
-                    className="btn btn-outline-primary btn-sm">Add more</button>
-                 } 
-                 {
-                  !isInCart(product) && 
-                    <button 
-                    onClick={() => addProduct(product)}
-                    className="btn btn-primary btn-sm">Add to cart</button>
-                }
+                <Link to="/" onClick={handleHide} className="btn btn-link btn-sm mr-2">Details</Link>
+
+                    { isInCart(product) && 
+                        <button 
+                        onClick={() => increase(product)}
+                        className="btn btn-outline-primary btn-sm">Add more</button>
+                    } 
+                    {
+                    !isInCart(product) && 
+                        <button 
+                        onClick={() => addProduct(product)}
+                        className="btn btn-primary btn-sm">Add to cart</button>
+                    }
 
                 
             </div>
